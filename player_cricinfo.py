8c7ebb423ba70bbc4ff449967dd2a7d9403684ca
Andre Hofmeyr <andre.hofmeyr@gmail.com>
2024-03-21 17:06:03 +0200

    player_cricinfo.py - loop over player IDs

--- player_cricinfo.py
+++ player_cricinfo.py
@@ -1,6 +1,9 @@
-from espncricinfo.player import Player
-p = Player('372116')
-p.name
-p.get_career_averages()
-p.get_career_summary()
-p.get_data()
+players = ["372116", "379143", "696401"]
+for x in players:
+    from espncricinfo.player import Player
+    print(x)
+    p = Player(x)
+    print(p.name)
+    p.get_career_averages()
+    p.get_career_summary()
+    p.get_data()
